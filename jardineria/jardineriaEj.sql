0612cbb2dffeb23e0b92809fdd7614bad2f3a479
Maria Delfina Samperi <delfisamperi@gmail.com>
2025-01-31 17:43:59 -0300

    repasos y jdbc conections

---
+++ jardineria/jardineriaEj.sql
@@ -0,0 +1,43 @@
+-- ✏️Ejercicios de Integración JARDINERÍA 
+--  Calcula la fecha del primer y último pago realizado por cada uno de los clientes. El listado deberá mostrar 
+-- el nombre y los apellidos de cada cliente.
+
+-- Lista las ventas totales de los productos que hayan facturado más de 3000 euros. Se mostrará el nombre, unidades
+-- vendidas, total facturado y total facturado con impuestos (21% IVA).
+
+--  Los clientes cuyo límite de crédito sea mayor que los pagos que haya realizado. (Sin utilizar INNER JOIN).
+
+-- Devuelve el nombre, los apellidos y el email de los empleados que están a cargo de Alberto Soria.
+
+-- Devuelve un listado que muestre solamente los clientes que sí han realizado ningún pago. (Recomendamos resolver
+-- este ejercicio con Subconsultas del tipo IN y NOT IN).
+
+-- Devuelve un listado de los productos (código y nombre)  que han aparecido en un pedido alguna vez.(Recomendamos
+-- resolver este ejercicio con Subconsultas del tipo EXISTS y NOT EXISTS).
+
+-- Devuelve el nombre del producto que tenga el precio de venta más caro. (Recomendamos resolver este ejercicio 
+-- con Subconsultas del tipo ALL y ANY).
+
+-- Devuelve el nombre del producto que tenga el precio de venta más caro (puedes realizarlo ordenando la tabla).
+
+-- Calcula la suma de la cantidad total de todos los productos que aparecen en cada uno de los pedidos.
+
+-- Devuelve el nombre del cliente con mayor límite de crédito.
+
+-- Devuelve el producto que más unidades tiene en stock.
+
+-- Devuelve un listado que muestre solamente los clientes que no han realizado ningún pago. (Recomendamos resolver 
+-- este ejercicio con Subconsultas del tipo ALL y ANY).
+
+-- Devuelve un listado de los productos que nunca han aparecido en un pedido. (Recomendamos resolver este ejercicio
+-- con Subconsultas del tipo EXISTS y NOT EXISTS).
+
+-- Calcula el número de productos diferentes que hay en cada uno de los pedidos. 
+
+-- Devuelve el nombre de los representantes de ventas y el número de clientes al que atiende cada uno.
+
+-- Calcula cuántos clientes tiene cada una de las ciudades que empiezan por M.
+
+-- Calcula el número de clientes que no tiene asignado representante de ventas.
+
+-- Devuelve un listado de los 20 productos más vendidos y el número total de unidades que se han vendido de cada uno. El listado deberá estar ordenado por el número total de unidades vendidas descendente.
